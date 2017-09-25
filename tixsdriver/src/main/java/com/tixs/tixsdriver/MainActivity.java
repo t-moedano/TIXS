685d2455da8cd6f7f8cbd4c493cbf78a5a370f6d
Fred-Valladao <fred.valladao@gmail.com>
2017-09-24 21:37:34 -0300

    Atributos de caixas de textos arrumados e mudança no layout do APP

--- tixsdriver/src/main/java/com/tixs/tixsdriver/MainActivity.java
+++ tixsdriver/src/main/java/com/tixs/tixsdriver/MainActivity.java
@@ -1,6 +1,8 @@
 package com.tixs.tixsdriver;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;

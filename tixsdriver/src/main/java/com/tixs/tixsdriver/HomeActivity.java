45840874914d08ab79671115cd769477f6aa1197
aline <aline.silandrade@gmail.com>
2017-09-24 23:33:47 -0300

    Home do driver adicionada.

--- tixsdriver/src/main/java/com/tixs/tixsdriver/HomeActivity.java
+++ tixsdriver/src/main/java/com/tixs/tixsdriver/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.tixs.tixsdriver;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void onAdicionarRota(View view) {
+        Intent i = new Intent(this, AdicionarEscolaActivity.class);
+        startActivity(i);
     }
 }

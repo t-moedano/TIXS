64aaa92cac0c9def1e0c38a97cfad265bad87e46
aline <aline.silandrade@gmail.com>
2017-10-26 01:50:52 -0200

    Implementação do cancelamento parte 1- mudança em tds as crinaças

--- tixsparents/src/main/java/com/tixs/tixsparents/CancelarIdaActivity.java
+++ tixsparents/src/main/java/com/tixs/tixsparents/CancelarIdaActivity.java
@@ -9,8 +9,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.firebase.database.FirebaseDatabase;
 import com.tixs.database.Crianca;
 import com.tixs.database.Responsavel;
 
@@ -39,6 +42,27 @@ public class CancelarIdaActivity extends AppCompatActivity {
 
         ArrayAdapter = new ArrayAdapter(getApplicationContext(), R.layout.check_text_view, HomeActivity.responsavelLogado.criancas);
         filhoListView.setAdapter(ArrayAdapter);
+        for (int i = 0; i < HomeActivity.responsavelLogado.criancas.size(); i++) {
+            Crianca c = HomeActivity.responsavelLogado.criancas.get(i);
+            filhoListView.setItemChecked(i, c.confirma_ida);
+        }
+
+        filhoListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Crianca c = HomeActivity.responsavelLogado.criancas.get(position);
+                c.confirma_ida = !c.confirma_ida;
+            }
+        });
+
+    }
+
+    public void bntCancelar(View view) {
+        for(Crianca c: HomeActivity.responsavelLogado.criancas) {
+            FirebaseDatabase.getInstance().getReference("criancas").child(c.id).setValue(c);
+        }
+
+
 
     }
 }

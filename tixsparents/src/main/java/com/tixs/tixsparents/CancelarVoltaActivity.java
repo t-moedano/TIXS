124ac53f30221241e8b90adb8371810fc13b14df
Pedro Dousseau <pdousseaumelo@gmail.com>
2017-11-20 17:52:27 -0200

    Cancelamento da volta finalizado.

--- tixsparents/src/main/java/com/tixs/tixsparents/CancelarVoltaActivity.java
+++ tixsparents/src/main/java/com/tixs/tixsparents/CancelarVoltaActivity.java
@@ -25,7 +25,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
- * Created by aline on 13/10/17.
+ * Created by dousseaux on 13/11/17.
  */
 
 public class CancelarVoltaActivity extends AppCompatActivity {
@@ -38,18 +38,17 @@ public class CancelarVoltaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cancelar_ida);
+        setContentView(R.layout.activity_cancelar_volta);
         ButterKnife.bind(this);
-
-//        filhoListView = (ListView) findViewById(R.id.filhoListView);
 
         filhoListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
         criancaArrayAdapter = new ArrayAdapter<Crianca>(getApplicationContext(), R.layout.check_text_view, HomeActivity.responsavelLogado.criancas);
         filhoListView.setAdapter(criancaArrayAdapter);
+        
         for (int i = 0; i < HomeActivity.responsavelLogado.criancas.size(); i++) {
             Crianca c = HomeActivity.responsavelLogado.criancas.get(i);
-            podeCancelar[i] = c.confirma_ida;
+            podeCancelar[i] = c.confirma_volta;
             filhoListView.setItemChecked(i, false);
 
         }
@@ -59,10 +58,10 @@ public class CancelarVoltaActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Crianca c = criancaArrayAdapter.getItem(position);
                 if (podeCancelar[position])
-                    c.confirma_ida = !c.confirma_ida;
+                    c.confirma_volta = !c.confirma_volta;
                 else {
                     view.setEnabled(false);
-                    Toast.makeText(getApplicationContext(), "Essa crianca ja nao vai", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Essa crianca já não volta", Toast.LENGTH_LONG).show();
                 }
             }
         });

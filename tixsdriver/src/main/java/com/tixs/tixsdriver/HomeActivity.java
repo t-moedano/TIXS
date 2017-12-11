bcef9637808499ca4e118aed16e67f49ae937be8
pedronaresi <pmnaresi@hotmail.com>
2017-12-11 02:08:53 -0200

    Terminamos uhul

--- tixsdriver/src/main/java/com/tixs/tixsdriver/HomeActivity.java
+++ tixsdriver/src/main/java/com/tixs/tixsdriver/HomeActivity.java
@@ -2,7 +2,12 @@ package com.tixs.tixsdriver;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -23,10 +28,55 @@ public class HomeActivity extends AppCompatActivity {
     Button adiministrarVanButton;
     Button addBanButton;
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToogle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToogle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToogle);
+        mToogle.syncState();
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // method invoked only when the actionBar is not null.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_menu);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem){
+                switch (menuItem.getItemId()){
+                    case(R.id.iniciarIdaButton1):
+                        Intent accountActivity = new Intent(getApplicationContext(), CheckInIdaActivity.class);
+                        startActivity(accountActivity);
+                        break;
+                    case(R.id.btnIniciarVoltar1):
+                        Intent accountActivity2 = new Intent(getApplicationContext(), CheckInVoltaActivity.class);
+                        startActivity(accountActivity2);
+                        break;
+                    case(R.id.adiministrarVanButton1):
+                        Intent accountActivity3 = new Intent(getApplicationContext(), AdministrarVanActivity.class);
+                        startActivity(accountActivity3);
+                        break;
+                    case(R.id.addBanButton1):
+                        Intent accountActivity4 = new Intent(getApplicationContext(), CreateVanActivity.class);
+                        startActivity(accountActivity4);
+                        break;
+                    case(R.id.nav_locateVan1):
+                        Intent accountActivity5 = new Intent(getApplicationContext(), LocateVanActivity.class);
+                        startActivity(accountActivity5);
+                        break;
+                }
+                return true;
+            }
+        });
 
         iniciarIdaButton = (Button) findViewById(R.id.iniciarIdaButton);
         adiministrarVanButton = (Button) findViewById(R.id.adiministrarVanButton);
@@ -50,6 +100,7 @@ public class HomeActivity extends AppCompatActivity {
                         iniciarIdaButton.setEnabled(true);
                         adiministrarVanButton.setEnabled(true);
                         addBanButton.setEnabled(true);
+
                     }
 
                     @Override
@@ -104,5 +155,13 @@ public class HomeActivity extends AppCompatActivity {
     {
         Intent i = new Intent(this, CheckInVoltaActivity.class);
         startActivity(i);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToogle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
